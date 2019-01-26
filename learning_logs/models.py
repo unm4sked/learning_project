d657402f9b615350376d058e42ff9a81331cbcef
unm4sked <lukasz.k.kalicki@gmail.com>
2019-01-26 16:19:32 +0100

    review changes

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -6,14 +6,16 @@ class Topic(models.Model):
     """Tematy poznawane przez usera"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User,on_delete=models.CASCADE,)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, )
+
     def __str__(self):
         """Zwraza reprezentacje modelu w postaci stringa"""
         return self.text
 
+
 class Entry(models.Model):
     """Konkretne informacje o postepach w nauce"""
-    topic = models.ForeignKey('Topic',on_delete=models.CASCADE)
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
@@ -25,4 +27,3 @@ class Entry(models.Model):
         if len(self.text) > 50:
             return self.text[:50] + "..."
         return self.text
-
